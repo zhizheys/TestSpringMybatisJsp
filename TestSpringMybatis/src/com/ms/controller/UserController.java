b2a24689baff15afe081b6a3a43453fa620b3615
jliu14 <jean.liu@morningstar.com>
2018-08-21 13:46:14 +0800

    add commons-logging and log4j to write log info

--- TestSpringMybatis/src/com/ms/controller/UserController.java
+++ TestSpringMybatis/src/com/ms/controller/UserController.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.jasper.tagplugins.jstl.core.Redirect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
@@ -31,170 +33,176 @@ import com.ms.service.UserService;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-	
+
+	private final static Log logger = LogFactory.getLog(UserController.class);
+
 	@Autowired
 	private UserService userService;
-	
-	//对表单提交的日期进行格式化
-	@InitBinder  
-	public void initBinder(WebDataBinder binder) {  
-	    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");  
-	    dateFormat.setLenient(false);  
-	    binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));  
+
+	// 对表单提交的日期进行格式化
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		dateFormat.setLenient(false);
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
 	}
-	
-   @RequestMapping(value="/show",method=RequestMethod.GET)
-   @ResponseBody
+
+	@RequestMapping(value = "/show", method = RequestMethod.GET)
+	@ResponseBody
 	public String show() {
-	   int userId=3;
-	   String result="hello world";
-	   User user = new User();
-       user = userService.getUserById(userId);
-	   
-	   ApiResult apiResult = new ApiResult();
-	   apiResult.setResultCode(200);
-	   apiResult.setMessage("success");
-	   apiResult.setData(user);
-	   
-	  ObjectMapper objectMapper =new ObjectMapper();
-	  
-	  try{
-		  result = objectMapper.writeValueAsString(apiResult);
-	  }catch(Exception ex){
-		  result = ex.getMessage();
-	  }
+		int userId = 3;
+		String result = "hello world";
+		User user = new User();
+		user = userService.getUserById(userId);
 
-	   return result;
+		ApiResult apiResult = new ApiResult();
+		apiResult.setResultCode(200);
+		apiResult.setMessage("success");
+		apiResult.setData(user);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		try {
+			result = objectMapper.writeValueAsString(apiResult);
+		} catch (Exception ex) {
+			result = ex.getMessage();
+		}
+
+		logger.debug("DEBUG ...............................");
+		logger.info("INFO ................................."+ "user/show controller info");
+		logger.error("ERROR ..............................." + "user/show controller error");
+
+		return result;
 	}
-   
-   //传统的url传递参数 http://localhost:8080/TestSpringMybatis/user/userinfo?id=10
-   @RequestMapping(value="/userinfo",method=RequestMethod.GET)
-   @ResponseBody
+
+	// 传统的url传递参数 http://localhost:8080/TestSpringMybatis/user/userinfo?id=10
+	@RequestMapping(value = "/userinfo", method = RequestMethod.GET)
+	@ResponseBody
 	public ApiResult getUserById(@RequestParam("id") int id) {
-	   int userId=id;
-	   User user = new User();
-       user = userService.getUserById(userId);
-	   
-	   ApiResult apiResult = new ApiResult();
-	   apiResult.setResultCode(200);
-	   apiResult.setMessage("success");
-	   apiResult.setData(user);
-	   
-	   return apiResult;
+		int userId = id;
+		User user = new User();
+		user = userService.getUserById(userId);
+
+		ApiResult apiResult = new ApiResult();
+		apiResult.setResultCode(200);
+		apiResult.setMessage("success");
+		apiResult.setData(user);
+
+		return apiResult;
 	}
-   
-   @RequestMapping(value="/userinfotwo",method=RequestMethod.GET)
-   @ResponseBody
+
+	@RequestMapping(value = "/userinfotwo", method = RequestMethod.GET)
+	@ResponseBody
 	public ApiResult getUserById() {
-	   int userId=10;
-	   User user = new User();
-       user = userService.getUserById(userId);
-	   
-	   ApiResult apiResult = new ApiResult();
-	   apiResult.setResultCode(200);
-	   apiResult.setMessage("success");
-	   apiResult.setData(user);
-	   
-	   return apiResult;
-  }
-   
-   //restful url 风格http://localhost:8080/TestSpringMybatis/user/userinfo2/10
-   @RequestMapping(value="/userinfo2/{id}",method=RequestMethod.GET)
-   @ResponseBody
+		int userId = 10;
+		User user = new User();
+		user = userService.getUserById(userId);
+
+		ApiResult apiResult = new ApiResult();
+		apiResult.setResultCode(200);
+		apiResult.setMessage("success");
+		apiResult.setData(user);
+
+		return apiResult;
+	}
+
+	// restful url 风格http://localhost:8080/TestSpringMybatis/user/userinfo2/10
+	@RequestMapping(value = "/userinfo2/{id}", method = RequestMethod.GET)
+	@ResponseBody
 	public String getUserById2(@PathVariable("id") int id) {
-	   int userId=id;
-	   String result=null;
-	   User user = new User();
-       user = userService.getUserById(userId);
-	   
-	   ApiResult apiResult = new ApiResult();
-	   apiResult.setResultCode(200);
-	   apiResult.setMessage("success");
-	   apiResult.setData(user);
-	   
-	  ObjectMapper objectMapper =new ObjectMapper();
-	  
-	  try{
-		  result = objectMapper.writeValueAsString(apiResult);
-	  }catch(Exception ex){
-		  result = ex.getMessage();
-	  }
+		int userId = id;
+		String result = null;
+		User user = new User();
+		user = userService.getUserById(userId);
 
-	   return result;
+		ApiResult apiResult = new ApiResult();
+		apiResult.setResultCode(200);
+		apiResult.setMessage("success");
+		apiResult.setData(user);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		try {
+			result = objectMapper.writeValueAsString(apiResult);
+		} catch (Exception ex) {
+			result = ex.getMessage();
+		}
+
+		return result;
 	}
 
-   @RequestMapping(value="/adduser",method=RequestMethod.POST)
-    public String addUser(@ModelAttribute UserViewModel user) {
-       ModelAndView modelAndView = new ModelAndView();
-	   Date date = new Date();
-	   
-	   User addUser = new User();
-	   addUser.setUserName(user.getUserName());
-	   addUser.setUserAge(user.getUserAge());
-	   addUser.setUserAddress(user.getUserAddress());
-	   addUser.setUserPassword(user.getUserPassword());
-	   addUser.setUserBirthday(user.getUserBirthday());
-	   
-	  int userId=userService.addUser(addUser);
-      User userFind = userService.getUserById(userId);
-	  System.out.println("==============================add user id is :" + userFind.getUserId()); 
-      
-	  return "redirect:/user/showalluser";
+	@RequestMapping(value = "/adduser", method = RequestMethod.POST)
+	public String addUser(@ModelAttribute UserViewModel user) {
+		ModelAndView modelAndView = new ModelAndView();
+		Date date = new Date();
+
+		User addUser = new User();
+		addUser.setUserName(user.getUserName());
+		addUser.setUserAge(user.getUserAge());
+		addUser.setUserAddress(user.getUserAddress());
+		addUser.setUserPassword(user.getUserPassword());
+		addUser.setUserBirthday(user.getUserBirthday());
+
+		int userId = userService.addUser(addUser);
+		User userFind = userService.getUserById(userId);
+		System.out.println("==============================add user id is :" + userFind.getUserId());
+
+		return "redirect:/user/showalluser";
 
 	}
-    
-   @RequestMapping(value="/addusertwo",method=RequestMethod.POST)
-   @ResponseBody
-   public ApiResult addUserTwo(@ModelAttribute UserViewModel user) {
-       ModelAndView modelAndView = new ModelAndView();
-	   Date date = new Date();
-	   
-	   User addUser = new User();
-	   addUser.setUserName(user.getUserName());
-	   addUser.setUserAge(user.getUserAge());
-	   addUser.setUserAddress(user.getUserAddress());
-	   addUser.setUserPassword(user.getUserPassword());
-	   addUser.setUserBirthday(user.getUserBirthday());
-	   
-	  int userId=userService.addUser(addUser);
-	  
-	  String result="";
-	  HashMap hm = new HashMap();
-	  hm.put("userId", userId);
-	  
-	  ApiResult apiResult = new ApiResult();
-	   apiResult.setResultCode(200);
-	   apiResult.setMessage("success");
-	   apiResult.setData(hm);
-	   
-	  return apiResult;
-	}
-   
-   @RequestMapping(value="/registeruser",method=RequestMethod.GET)
-    public ModelAndView registerUser() {
-	   ModelAndView modelAndView = new ModelAndView();
-	   modelAndView.setViewName("user/registeruser");
 
-	   return modelAndView;
+	@RequestMapping(value = "/addusertwo", method = RequestMethod.POST)
+	@ResponseBody
+	public ApiResult addUserTwo(@ModelAttribute UserViewModel user) {
+		ModelAndView modelAndView = new ModelAndView();
+		Date date = new Date();
+
+		User addUser = new User();
+		addUser.setUserName(user.getUserName());
+		addUser.setUserAge(user.getUserAge());
+		addUser.setUserAddress(user.getUserAddress());
+		addUser.setUserPassword(user.getUserPassword());
+		addUser.setUserBirthday(user.getUserBirthday());
+
+		int userId = userService.addUser(addUser);
+
+		String result = "";
+		HashMap hm = new HashMap();
+		hm.put("userId", userId);
+
+		ApiResult apiResult = new ApiResult();
+		apiResult.setResultCode(200);
+		apiResult.setMessage("success");
+		apiResult.setData(hm);
+
+		return apiResult;
 	}
 
-   @RequestMapping(value="/showalluser",method=RequestMethod.GET)
+	@RequestMapping(value = "/registeruser", method = RequestMethod.GET)
+	public ModelAndView registerUser() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("user/registeruser");
+
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/showalluser", method = RequestMethod.GET)
 	public ModelAndView showAllUser() {
 
-	   ModelAndView result= new ModelAndView();
-	   User user = new User();
-	   List<User> userList = userService.getAllUser();
-	   
-	  result.addObject("userList", userList);
-      result.setViewName("user/alluser");
-	   return result;
+		ModelAndView result = new ModelAndView();
+		User user = new User();
+		List<User> userList = userService.getAllUser();
+
+		result.addObject("userList", userList);
+		result.setViewName("user/alluser");
+		return result;
 	}
-   
-   @RequestMapping(value="/dealuser",method=RequestMethod.GET)
+
+	@RequestMapping(value = "/dealuser", method = RequestMethod.GET)
 	public ModelAndView dealUser() {
-	   ModelAndView result= new ModelAndView();
-       result.setViewName("user/userinfo");
-	   return result;
+		ModelAndView result = new ModelAndView();
+		result.setViewName("user/userinfo");
+		return result;
 	}
-   
+
 }
